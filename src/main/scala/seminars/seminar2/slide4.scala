e76583943ff2f0b7609803dd0c21276f32821793
valentiay <valentiay@yandex.ru>
2020-09-21 16:04:08 +0300

    Seminar 2 fix project compilation

--- src/main/scala/seminars/seminar2/slide4.scala
+++ src/main/scala/seminars/seminar2/slide4.scala
@@ -1,30 +1,30 @@
 package seminars.seminar2
 
-// Пример части методов, которые генерируются для
-// final case class Name(first: String, last: String)
-
-final class Name(val first: String, val last: String) {
-  override def toString: String =
-    s"Name($first, $last)"
-
-  override def equals(obj: Any): Boolean =
-    obj match {
-      case other: Name =>
-        other.first == first && other.last == last
-      case _ =>
-        false
-    }
-
-  def copy(first: String = first, last: String = last): Name =
-    new Name(first, last)
-}
-
-object Name {
-  def apply(first: String, last: String) =
-    new Name(first, last)
-}
-
 object slide6 extends App {
+
+  // Пример части методов, которые генерируются для
+  // final case class Name(first: String, last: String)
+  final class Name(val first: String, val last: String) {
+    override def toString: String =
+      s"Name($first, $last)"
+
+    override def equals(obj: Any): Boolean =
+      obj match {
+        case other: Name =>
+          other.first == first && other.last == last
+        case _ =>
+          false
+      }
+
+    def copy(first: String = first, last: String = last): Name =
+      new Name(first, last)
+  }
+
+  object Name {
+    def apply(first: String, last: String) =
+      new Name(first, last)
+  }
+
   val name1 = Name("Vasia", "Pupkin")
   val name2 = Name("Alexander", "Valentinov")
   println(name1)
