c464d0ee860e37637601f0de86b51bb835350a3d
valentiay <valentiay@yandex.ru>
2020-09-22 13:43:30 +0300

    Seminar 3 erasure

--- src/main/scala/seminars/seminar3/slide6.scala
+++ src/main/scala/seminars/seminar3/slide6.scala
@@ -1,80 +1,143 @@
 package seminars.seminar3
 
-case class Error(message: String)
-
-object eitherBasics extends App {
-
-  // Either[+A, +B] - тип, который содержит в себе либо значение типа A, либо значение типа B.
+object optionBasics extends App {
+  // Option[+A] - тип для обозначения опциональных значний.
   // Имеет две реализации:
-  //  * Left[+A, +B] - содержит в себе значение типа A (тот, что слева)
-  //  * Right[+A, +B] - содержит в себе значение типа B (тот, что справа)
-  // Наиболее частое использование Either - обработка ошибок. Тип A (левый) - это ошибка,
-  // а тип B (правый) - значение, полученное при успешном выполнении
-  val errorOrSuccess1: Either[Error, String] = Right("I'm a string")
-  val errorOrSuccess2: Either[Error, String] = Left(Error("something gone wrong"))
-  val errorOrSuccess3: Error Either String   = Left(Error("something gone wrong again"))
-  println(errorOrSuccess1)
-  println(errorOrSuccess2)
-  println(errorOrSuccess3)
+  //  * Some[+A] - для случаев, когда значение есть
+  //  * None - для случаев, когда значения нет
+  val maybeString1: Option[String] = Some("42")
+  val maybeString2: Option[String] = None
+
+  println(maybeString1)
+  println(maybeString2)
   println()
 
 
-  // Either, как и Option, это sealed-class. Его тоже можно испльзовать в pattern-matching.
-  def handleError(errorOrSucess: Either[Error, String]): Unit =
-    errorOrSucess match {
-      case Right(value)         => println(s"Success: $value")
-      case Left(Error(message)) => println(s"Error occurred: $message")
+  // Т.к. Option - это sealed class, при работе с ним можно использовать pattern-matching
+  def handleOption[A](maybeA: Option[A]): Unit =
+    maybeA match {
+      case Some(a) => println(s"Got something: $a")
+      case None => println("Got nothing")
     }
 
-  handleError(errorOrSuccess1)
-  handleError(errorOrSuccess2)
-}
-
-object eitherTransform extends App {
-  // "Правое" значение Either можно преобразовать методом .map
-  val reverse: String => String =
-    str => str.reverse
-  println("map")
-  println(Right("123").map(reverse))
-  println(Left("error!!!").map(reverse))
+  println("pattern matching")
+  handleOption(Some("42"))
+  handleOption(None)
   println()
 
 
-  // "Правое" значение можно преобразовать и функцией, которая возвращает Either, с помощью .flatMap
-  val toInt: String => Either[Error, Int] =
-    str => str.toIntOption.toRight(Error("It's not an Int"))
+  // Option может обрабатывать null-pointer:
+  println("null")
+  println(Option("Not null"))
+  println(Option(null))
+  println()
+
+}
+
+object optionTransform extends App {
+  // Значение внутри Option можно изменить методом map
+  val getAnswer: String => String =
+    str => s"The answer is $str"
+
+  println("map")
+  println(Some("42").map(getAnswer))
+  println(None.map(getAnswer))
+  println()
+
+
+  // Также, для преобразований можно использовать функции, которые возвращают Option
+  val toIntOption: String => Option[Int] =
+    str => str.toIntOption
 
   println("flatMap")
-  println(Right("123").flatMap(toInt))
-  println(Right("Not an int").flatMap(toInt))
-  println(Left(Error("It's already an error")).flatMap(toInt))
+  println(Some("42").flatMap(toIntOption))
+  println(Some("I am not a number").flatMap(toIntOption))
+  println(None.flatMap(toIntOption))
   println()
 
-  // Either можно использовать в for-comprehension
-  def getAmountFromPrice(price: String): Either[Error, String] =
-    price.split(' ') match {
-      case Array(amount, currency) => Right(amount)
-      case _                       => Left(Error(s"'$price' is not a price"))
-    }
 
-  def parseAmount(price: String): Either[Error, Int] =
+  // Значения внутри Option можно фильтровать
+  val hasTwoChars: String => Boolean =
+    str => str.length == 2
+
+  println("filter")
+  println(Some("42").filter(hasTwoChars))
+  println(Some("123").filter(hasTwoChars))
+  println()
+
+
+  // Т.к. есть методы map и flatMap, Option можно использовать в for-comprehension
+  def firstCharToDoubledInt(str: String): Option[Int] =
     for {
-      amountString <- getAmountFromPrice(price)
-      amount       <- toInt(amountString)
-    } yield amount
+      firstChar <- str.headOption
+      int <- firstChar.toString.toIntOption
+    } yield 2 * int
 
-  println("for-comprehension")
-  println(parseAmount("1 EUR"))
-  println(parseAmount("notanamount"))
-  println(parseAmount("one EUR"))
+  println(firstCharToDoubledInt("12345"))
+  println(firstCharToDoubledInt("hello"))
+
+  def firstCharToDoubledIntUnsugared(str: String): Option[Int] =
+    str.headOption
+      .flatMap(firstChar => firstChar.toString.toIntOption)
+      .map(int => 2 * int)
+
+  println(firstCharToDoubledIntUnsugared("12345"))
+  println(firstCharToDoubledIntUnsugared("hello"))
 }
 
-object eitherCheck extends App {
-  println("isRight/isLeft")
-  println(Right("123").isRight)
-  println(Right("123").isLeft)
+object optionCheck extends App {
+  // Можно проверять Option на пустоту
+  println("isDefined/isEmpty")
+  println(Some("42").isEmpty)
+  println(None.isEmpty)
+  println(Some("42").isDefined)
+  println(None.isDefined)
+  println()
 
-  println(Right("123").contains("123"))
-  println(Left("123").contains("123"))
-  // exists, forall etc.
+
+  // Можно проверять, что находится внутри
+  println("contains")
+  println(Some("42").contains("42"))
+  println(Some("123").contains("42"))
+  println(None.contains("42"))
+  println()
+
+
+  // Есть методы forall и exists. Они отличаются тем, что у None метод forall возвращает true, а exists - false
+  val hasTwoChars: String => Boolean =
+    str => str.length == 2
+
+  println("forall/exists")
+  println(Some("42").exists(hasTwoChars))
+  println(Some("42").forall(hasTwoChars))
+  println(None.forall(hasTwoChars))
+  println(None.exists(hasTwoChars))
+}
+
+object optionGet extends App {
+  // Метод .get возвращает значение, если оно есть, но может бросить исключение
+  println("get")
+  println(Option("42").get)
+  // println(None.get) // Exception!!!
+  println()
+
+
+  // Лучше использовать метод .getOrElse, в котором можно указать значение по умолчанию. Он не бросает исключения
+  println("getOrElse")
+  println(Option("42").getOrElse("123"))
+  println(None.getOrElse("123"))
+  println()
+
+
+  // На случай, если значение по умолчанию тоже опциональное, есть метод .orElse
+  val maybeDefault1: Option[String] = Some("123")
+  val maybeDefault2: Option[String] = None
+  println("orElse")
+  println(None.orElse(maybeDefault1))
+  println(None.orElse(maybeDefault2))
+  println()
+
+  // Использовать значение, находящееся внутри Option, для side-эффекта можно с помощью метода .foreach
+  Some("123").foreach(println)
+  None.foreach(println)
 }
